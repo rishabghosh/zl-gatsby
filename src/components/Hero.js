4d85ff0e3a5b2d99644f4b715dd0cfc65d4126f1
rishabghosh <rishabg7269@gmail.com>
2022-05-21 19:04:28 +0530

    Change banner and logo + code formatting.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,35 @@
-import React from 'react'
-
-import { StaticImage } from 'gatsby-plugin-image'
+import React from "react";
 
 const Hero = ({ showPerson }) => {
   return (
     <header className="hero">
-      {showPerson && (
-        <StaticImage
-          src="../assets/person.png"
-          placeholder="blurred"
-          className="hero-person"
-          alt="person typing"
+      {/*{showPerson && (*/}
+      {/*  <StaticImage*/}
+      {/*    src="../assets/person.png"*/}
+      {/*    placeholder="blurred"*/}
+      {/*    className="hero-person"*/}
+      {/*    alt="person typing"*/}
+      {/*  />*/}
+      {/*)}*/}
+      <div className={"hero-person-wrapper"}>
+        <img
+          src="https://static.wixstatic.com/media/736ab9_523a6bdffb1f4e67893af662307d304b~mv2.gif"
+          alt="panda"
+          className={"hero-person"}
+          placeholder={"blurred"}
         />
-      )}
+      </div>
+      <div className="promotions">
+        <p>welcome to cbcs zoology .org</p>
+        <p>
+          This website is created to help the students those who are having ZOOLOGY in graduation under this new CBCS
+          system.
+          This website for the first time in India is going to provide topic wise information in question - answer
+          format under the guidance of experienced professors.
+        </p>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,17 @@ import { FaBars } from 'react-icons/fa'
 import logo from '../assets/logo.svg'
 import Links from '../constants/links'
 import SocialLinks from '../constants/socialLinks'
+
+const logoSource = "https://static.wixstatic.com/media/736ab9_bd532081d9e24a35bce6a0a0e53cbd8c~mv2.png/v1/crop/x_0,y_38,w_2000,h_424/fill/w_1000,h_212,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/736ab9_bd532081d9e24a35bce6a0a0e53cbd8c~mv2.png";
+
 const Navbar = ({ toggle }) => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <Link to="/" className="nav-logo">
-            <img src={logo} alt="mdx logo" />
+            {/*<img src={logo} alt="mdx logo" />*/}
+            <img src={logoSource} alt="cbs zoology logo" />
           </Link>
           <button className="toggle-btn" onClick={toggle}>
             <FaBars></FaBars>
