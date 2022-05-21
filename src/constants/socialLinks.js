862bb9ec905187ff534bf263313ba9cbd2384d37
rishabghosh <rishabg7269@gmail.com>
2022-05-21 21:49:07 +0530

    Add more social media links.

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,28 +1,35 @@
-import React from "react"
-import {
-  FaFacebookSquare,
-  FaDribbbleSquare,
-  FaTwitterSquare,
-} from "react-icons/fa"
+import React from "react";
+import { FaFacebook, FaInstagram, FaTelegram, FaTwitter, FaWhatsapp } from "react-icons/fa";
+
 const SocialLinks = ({ styleClass }) => {
   return (
     <ul className={styleClass}>
       <li>
-        <a href="https://twitter.com">
-          <FaFacebookSquare className="social-icon facebook-icon"></FaFacebookSquare>
+        <a href="https://facebook.com">
+          <FaFacebook className="social-icon facebook-icon"></FaFacebook>
         </a>
       </li>
       <li>
         <a href="https://twitter.com">
-          <FaDribbbleSquare className="social-icon dribble-icon"></FaDribbbleSquare>
+          <FaTwitter className="social-icon twitter-icon"></FaTwitter>
         </a>
       </li>
       <li>
-        <a href="https://twitter.com">
-          <FaTwitterSquare className="social-icon twitter-icon"></FaTwitterSquare>
+        <a href="https://instagram.com">
+          <FaInstagram className="social-icon instagram-icon"></FaInstagram>
+        </a>
+      </li>
+      <li>
+        <a href="https://web.whatsapp.com">
+          <FaWhatsapp className="social-icon whatsapp-icon"></FaWhatsapp>
+        </a>
+      </li>
+      <li>
+        <a href="https://telegram.org/">
+          <FaTelegram className="social-icon telegram-icon"></FaTelegram>
         </a>
       </li>
     </ul>
-  )
-}
-export default SocialLinks
+  );
+};
+export default SocialLinks;
